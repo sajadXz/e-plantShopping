ce8cf05f3b988a15a3cfee861fe427e1b266358c
Z <s@gmail.com>
2026-08-19 04:39:51 -0400

    fixed the gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/shoppingreact",
-  base: "/YOUR_REPOSITORY_NAME",
+  base: "/e-plantShopping",
 
   plugins: [react()],
 })
